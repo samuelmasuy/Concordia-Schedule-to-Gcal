cfa1ca559f6af5ef374f3eecd52057a093833aa7
Samuel Masuy <samuel.masuy@gmail.com>
2014-04-06 02:33:47 -0400

    add config example

---
+++ config_example.py
@@ -0,0 +1,32 @@
+#  ===========================================================================
+#
+#  Copyright (C) 2014 Samuel Masuy. All rights reserved.
+#  samuel.masuy@gmail.com
+#
+#  This file is part of ScheduleToGoogleCal.
+#
+#  This file may be used under the terms of the GNU General Public
+#  License version 2.0 as published by the Free Software Foundation
+#  and appearing in the file LICENSE included in the packaging of
+#  this file.  Please review this information to ensure GNU
+#  General Public Licensing requirements will be met.
+#
+#  This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+#  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  For use of this library in commercial applications, please contact
+#  samuel.masuy@gmail.com
+#
+#  ===========================================================================
+# -*- coding: utf-8 -*-
+import os
+# Protects against cross-site request forgery
+CSRF_ENABLED = True
+
+SECRET_KEY = os.urandom(66)
+
+# CLIENT_ID = YOUR_CLIENT_ID
+# CLIENT_SECRET = YOUR_CLIENT_SECRET
