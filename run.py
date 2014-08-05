caf50e132c285832d67136c0819d7f4b58ed5092
Samuel Masuy <samuel.masuy@gmail.com>
2014-08-05 16:59:53 -0400

    Remove license headers

--- run.py
+++ run.py
@@ -1,27 +1,11 @@
+# -*- coding: utf-8 -*-
 #  ===========================================================================
 #
 #  Copyright (C) 2014 Samuel Masuy. All rights reserved.
 #  samuel.masuy@gmail.com
 #
-#  This file is part of ScheduleToGoogleCal.
-#
-#  This file may be used under the terms of the GNU General Public
-#  License version 2.0 as published by the Free Software Foundation
-#  and appearing in the file LICENSE included in the packaging of
-#  this file.  Please review this information to ensure GNU
-#  General Public Licensing requirements will be met.
-#
-#  This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-#  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#  For use of this library in commercial applications, please contact
-#  samuel.masuy@gmail.com
-#
 #  ===========================================================================
-# -*- coding: utf-8 -*-
+
 __author__ = 'Samuel Masuy'
 __email__ = 'samuel.masuy@gmail.com'
 
