3ec289e45b6ff20f5a7df8669d656394472790cc
Samuel Masuy <samuel.masuy@gmail.com>
2014-08-05 16:58:32 -0400

    Remove license headers

--- app/views.py
+++ app/views.py
@@ -4,23 +4,6 @@
 #  Copyright (C) 2014 Samuel Masuy. All rights reserved.
 #  samuel.masuy@gmail.com
 #
-#  This file is part of ScheduleToGoogleCal.
-#
-#  This file may be used under the terms of the GNU General Public
-#  License version 2.0 as published by the Free Software Foundation
-#  and appearing in the file LICENSE included in the packaging of
-#  this file.  Please review this information to ensure GNU
-#  General Public Licensing requirements will be met.
-#
-#  This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-#  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#  For use of this library in commercial applications, please contact
-#  samuel.masuy@gmail.com
-#
 #  ===========================================================================
 import os
 
---
+++ tests/test_schedule.py
@@ -0,0 +1,8 @@
+
+
+def test_integer():
+    assert 4 == 4
+
+@pytest.fixture
+def tree(urls):
+
