ec6eb359a68157699a7166b2a1a3767487012c4d
Samuel Masuy <samuel.masuy@gmail.com>
2014-09-21 17:05:09 -0400

    [academic_dates] University holidays

--- app/scheduletogcal/academic_dates.py
+++ app/scheduletogcal/academic_dates.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 #  ===========================================================================
 #
@@ -9,58 +10,42 @@
     Academic_dates.
     ~~~~~~~~~~~~~~
 
-    Start and end of the 2014-2015 academic terms.
-    Data manually fetched from:
-    http://www.concordia.ca/students/registration/term-dates-deadlines.html
-    TO-DO: Scrape the above website in order to get a dict just like the
-    one below.
+    Holidays
+    Data fetched from:
+    - http://www.concordia.ca/events/university-holidays.html
 
     :copyright: (c) 2014 by Samuel Masuy.
     :license: GNU version 2.0, see LICENSE for more details.
 """
-from datetime import date
+from datetime import datetime
 
-_ACADEMIC_DATES = {
-    "summer_4A": {
-        "date": (date(2014, 5, 7), date(2014, 6, 23)),
-        "withdraw": date(2014, 6, 4)
-    },
-    "summer_5B": {
-        "date": (date(2014, 5, 7), date(2014, 8, 19)),
-        "withdraw": date(2014, 7, 14)
-    },
-    "summer_6C": {
-        "date": (date(2014, 7, 7), date(2014, 8, 19)),
-        "withdraw": date(2014, 7, 31)
-    },
-    "summer_7D": {
-        "date": (date(2014, 7, 7), date(2014, 7, 25)),
-        "withdraw": date(2014, 7, 17)
-    },
-    "summer_8E": {
-        "date": (date(2014, 7, 7), date(2014, 8, 15)),
-        "withdraw": date(2014, 7, 30)
-    },
-    "summer_9F": {
-        "date": (date(2014, 7, 28), date(2014, 8, 15)),
-        "withdraw": date(2014, 8, 7)
-    },
-    "fall": {
-        "date": (date(2014, 9, 2), date(2014, 12, 1)),
-        "withdraw": date(2014, 10, 26)
-    },
-    "fall_winter": {
-        "date": (date(2014, 9, 2), date(2015, 4, 14)),
-        "withdraw": date(2015, 3, 8)
-    },
-    "winter": {
-        "date": (date(2015, 1, 7), date(2015, 4, 14)),
-        "withdraw": date(2015, 3, 8)
-    }
-}
+import requests
+from lxml import html
+
+URL_HOLIDAY = 'http://www.concordia.ca/events/university-holidays.html'
 
 
-def get_academic_dates(semester):
-    """returns 2 datetime.date instances representing the start
-    and ending days, of a specific academic semester."""
-    return _ACADEMIC_DATES[semester]['date']
+def make_tree(url, arg=''):
+    """Create a lxml tree from a url with possibly some arguments."""
+    response = requests.get('{0}{1}'.format(url, arg)).text
+    return html.fromstring(response)
+
+
+def get_general_holidays():
+    """Get all the dates of when the university is closed."""
+    tree = make_tree(URL_HOLIDAY)
+    table_data_with_title = tree.xpath("//td[@title]")
+
+    h_dates = []
+    for table_data in table_data_with_title:
+        j = table_data.getparent().getparent()
+        for k in j.getchildren()[0]:
+            month_year = k.text
+        if table_data.text:
+            day = table_data.text
+        else:
+            for tag in table_data.getchildren():
+                day = tag.text
+        h_dates.append(
+            datetime.strptime(' '.join([month_year, day]), '%B %Y %d'))
+    return h_dates
