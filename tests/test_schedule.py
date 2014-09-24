387fd3ca1c7944bf47e463f89f89460b168f9f25
Samuel Masuy <samuel.masuy@gmail.com>
2014-09-23 23:00:02 -0400

    test file

--- tests/test_schedule.py
+++ tests/test_schedule.py
@@ -1,8 +1,15 @@
+import sys, os
+testdir = os.path.dirname(__file__)
+srcdir = '../app/scheduletogcal'
+sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
 
-
+from course import Course
+c = Course()
+c.summary = "xxx xxx"
 def test_integer():
-    assert 4 == 4
+    assert c.summary == "xxx xxx"
 
-@pytest.fixture
-def tree(urls):
+#@pytest.fixture
+#def tree(urls):
+
 
