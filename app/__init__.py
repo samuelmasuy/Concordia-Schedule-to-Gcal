43bbb24b9feb54f2435ecc8c08cc3720e4795f89
Samuel Masuy <samuel.masuy@gmail.com>
2014-10-16 16:49:38 -0400

    flask debug toolbar

--- app/__init__.py
+++ app/__init__.py
@@ -23,11 +23,13 @@
 #
 #  ===========================================================================
 from flask import Flask
+# from flask_debugtoolbar import DebugToolbarExtension
 
 # Declare app object
 app = Flask(__name__)
 # tell flask where is the config file
 app.config.from_object('config')
-
+app.debug = True
+# toolbar = DebugToolbarExtension(app)
 
 from app import views
