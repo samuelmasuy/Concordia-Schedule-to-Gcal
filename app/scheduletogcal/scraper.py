5d861321a6ac639bd877338fef5406083fc777c7
Samuel Masuy <samuel.masuy@gmail.com>
2014-10-27 21:52:54 -0400

    code refactor, ical support

--- app/scheduletogcal/scraper.py
+++ app/scheduletogcal/scraper.py
@@ -16,6 +16,11 @@
 """
 from lxml import html
 import requests
+from pytz import timezone
+
+from icalendar import Calendar
+from icalendar import Event
+from dateutil import parser
 
 from course import Course
 from location import get_buildings_location
@@ -28,7 +33,7 @@ class ScheduleScraper(object):
         self.buildings = get_buildings_location()
         self.index_color = 0
         self.semester = semester
-
+        self.course_list = self.parse()
 
     def parse(self):
         """Return a list of all courses formatted from the html file."""
@@ -88,37 +93,65 @@ class ScheduleScraper(object):
             result = [self.index_color, course]
         return result, seen
 
-    def to_dict(self):
-        """This function takes all the data parsed and returns a dictionary
-        with all formated data needed to transmit to Google calendar."""
-        course_list = self.parse()
-        entries = []
-        for courses in course_list:
-            for course in courses:
-                entry = dict()
-                entry["summary"] = course.summary
-                # Type of class, its section and the professor that gives it.
-                entry["description"] = course.description
-                entry["location"] = course.location
-                entry["colorId"] = course.colorid
-                # Date of the first course of the year and time when
-                # the class begins.
-                start_dic = dict()
-                entry["start"] = start_dic
-                start_dic["dateTime"] = course.datetime[1]
-                start_dic["timeZone"] = 'America/Montreal'
-                # Date of the first course of the year and time when
-                # the class ends.
-                end_dic = dict()
-                entry["end"] = end_dic
-                end_dic["dateTime"] = course.datetime[2]
-                end_dic["timeZone"] = 'America/Montreal'
-                # Repeat events weekly until the end of the academic year.
-                entry["recurrence"] = [
-                    'RRULE:FREQ=WEEKLY;UNTIL={};BYDAY={}'.format(
-                        course.datetime[3], course.datetime[0])]
-                entries.append(entry)
-        return entries
+
+def to_gcal(course_list):
+    """This function takes all the data parsed and returns a dictionary
+    with all formated data needed to transmit to Google calendar."""
+    gcal = []
+    for courses in course_list:
+        for course in courses:
+            entry = dict()
+            entry["summary"] = course.summary
+            # Type of class, its section and the professor that gives it.
+            entry["description"] = course.description
+            entry["location"] = course.location
+            entry["colorId"] = course.colorid
+            # Date of the first course of the year and time when
+            # the class begins.
+            start_dic = dict()
+            entry["start"] = start_dic
+            start_dic["dateTime"] = course.datetime[1]
+            start_dic["timeZone"] = 'America/Montreal'
+            # Date of the first course of the year and time when
+            # the class ends.
+            end_dic = dict()
+            entry["end"] = end_dic
+            end_dic["dateTime"] = course.datetime[2]
+            end_dic["timeZone"] = 'America/Montreal'
+            # Repeat events weekly until the end of the academic year.
+            entry["recurrence"] = [
+                'RRULE:FREQ=WEEKLY;UNTIL={};BYDAY={}'.format(
+                    course.datetime[3], course.datetime[0])]
+            gcal.append(entry)
+    return gcal
+
+
+def to_ical(course_list):
+    """This function takes all the data parsed and returns an
+    icalendar."""
+    ical = Calendar()
+    current_tz = timezone("America/Montreal")
+    for courses in course_list:
+        for course in courses:
+            bydays = course.datetime[0].split(',')
+            for byday in bydays:
+                entry_ical = Event()
+                entry_ical.add('summary', course.summary)
+                entry_ical.add('description', course.description)
+                entry_ical.add('location', course.location)
+                dtstart = parser.parse(
+                    course.datetime[1]).replace(tzinfo=current_tz)
+                entry_ical.add('dtstart', dtstart)
+                dtend = parser.parse(
+                    course.datetime[2]).replace(tzinfo=current_tz)
+                entry_ical.add('dtend', dtend)
+                until = parser.parse(
+                    course.datetime[3]).replace(tzinfo=current_tz)
+                entry_ical.add('rrule', {'freq': 'weekly',
+                                         'until': until,
+                                         'byday': byday})
+                ical.add_component(entry_ical)
+    return ical
 
 
 def make_tree(url):
